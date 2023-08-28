925b6d5ce5c5f6ef990570550a6c9ac683dbd240
Bohdan Boiprav <122865615+bohdanboiprav@users.noreply.github.com>
2023-08-28 11:08:30 +0300

    Create homework 10.py

---
+++ Week 10/homework 10.py
@@ -0,0 +1,43 @@
+from collections import UserDict
+
+
+class Record:
+    def __init__(self, name, phone=None):
+        self.name = name
+        self.phones = []
+        if phone:
+            self.phones.append(phone)
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    pass
+
+
+if __name__ == '__main__':
+    name = Name('Bill')
+    phone = Phone('1234567890')
+    rec = Record(name, phone)
+    ab = AddressBook()
+    ab.add_record(rec)
+
+    assert isinstance(ab['Bill'], Record)
+    assert isinstance(ab['Bill'].name, Name)
+    assert isinstance(ab['Bill'].phones, list)
+    assert isinstance(ab['Bill'].phones[0], Phone)
+    assert ab['Bill'].phones[0].value == '1234567890'
+
+    print('All Ok)')
