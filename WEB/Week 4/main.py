f0eb24c1767481a82c28aedf0141e8c6a219698f
Bohdan Boiprav <122865615+bohdanboiprav@users.noreply.github.com>
2023-10-22 16:59:38 +0100

    Fixed issues

--- WEB/Week 4/main.py
+++ WEB/Week 4/main.py
@@ -1,16 +1,107 @@
-# This is a sample Python script.
+import urllib.parse
+import mimetypes
+from pathlib import Path
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+import logging
+import socket
+from threading import Thread
+import datetime
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+BASE_DIR = Path()
+BUFFER_SIZE = 1024
+HTTP_PORT = 3000
+HTTP_HOST = '0.0.0.0'
+SOCKET_HOST = '127.0.0.1'
+SOCKET_PORT = 5000
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class GoItFramework(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        route = urllib.parse.urlparse(self.path)
+        match route.path:
+            case '/':
+                self.send_html('index.html')
+            case '/message':
+                self.send_html('message.html')
+            case '/messagesent':
+                self.send_html('messagesent.html')
+            case _:
+                file = BASE_DIR.joinpath(route.path[1:])
+                if file.exists():
+                    self.send_static(file)
+                else:
+                    self.send_html('error.html', 404)
+
+    def do_POST(self):
+        data = self.rfile.read(int(self.headers['Content-Length']))
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        client_socket.sendto(data, (SOCKET_HOST, SOCKET_PORT))
+        client_socket.close()
+        self.send_response(302)
+        self.send_header('Location', '/messagesent')
+        self.end_headers()
+
+    def send_html(self, filename, status_code=200):
+        self.send_response(status_code)
+        self.send_header('Content-Type', 'text/html')
+        self.end_headers()
+        with open(filename, 'rb') as file:
+            self.wfile.write(file.read())
+
+    def send_static(self, filename, status_code=200):
+        self.send_response(status_code)
+        mime_type, *_ = mimetypes.guess_type(filename)
+        if mime_type:
+            self.send_header('Content-Type', mime_type)
+        else:
+            self.send_header('Content-Type', 'text/plain')
+        self.end_headers()
+        with open(filename, 'rb') as file:
+            self.wfile.write(file.read())
 
 
-# Press the green button in the gutter to run the script.
+def save_data_from_client(data):
+    data_parse = urllib.parse.unquote_plus(data.decode())
+    try:
+        data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
+        data_time_of_request = {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%s"): data_dict}
+        with open('data/data. json', 'a', encoding='utf-8') as file:
+            json.dump(data_time_of_request, file, ensure_ascii=False, indent=4)
+    except ValueError as err:
+        logging.error(err)
+    except OSError as err:
+        logging.error(err)
+
+
+def run_socket_server(host, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind((host, port))
+    logging.info(f'Starting Socket server at {host}:{port}')
+    try:
+        while True:
+            msg, address = server_socket.recvfrom(BUFFER_SIZE)
+            logging.info(f'Socket received {address}: {msg.decode()}')
+            save_data_from_client(msg)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server_socket.close()
+
+
+def run_http_server(host, port):
+    http_server = HTTPServer((host, port), GoItFramework)
+    logging.info(f'Starting HTTP server')
+    try:
+        http_server.serve_forever()
+    except KeyboardInterrupt:
+        http_server.server_close()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
+    http_server = Thread(target=run_http_server, args=(HTTP_HOST, HTTP_PORT))
+    http_server.start()
+    socket_server = Thread(target=run_socket_server, args=(SOCKET_HOST, SOCKET_PORT))
+    socket_server.start()
