b7bd0b149cd2843ccb4140d873f57b7e6c24be0e
Bohdan Boiprav <122865615+bohdanboiprav@users.noreply.github.com>
2024-01-01 02:02:20 +0000

    Update auth.py

--- WEB/Week 13/Week 13 HW/src/services/auth.py
+++ WEB/Week 13/Week 13 HW/src/services/auth.py
@@ -14,7 +14,7 @@ from src.repository import users as repository_users
 
 class Auth:
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-    SECRET_KEY = "974790aec4ac460bdc11645decad4dce7c139b7f2982b7428ec44e886ea588c6"
+    SECRET_KEY = "HIDDEN"
     ALGORITHM = "HS256"
     oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
 
